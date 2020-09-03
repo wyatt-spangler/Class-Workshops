c90662e25fed1d68a78ffbf48a27ce9532795142
Wyatt Spangler <wyatt.spa@outlook.com>
2020-09-03 13:47:23 -0400

    Add project files.

---
+++ Class Workshops/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Workshops
+{
+    public class HoomanClass
+    {
+        // Attributes
+        public string name;
+        public int age;
+        public bool hasDog;
+        public string dogName;
+
+        //Methods
+
+        public void Greeting() 
+        {
+            if (hasDog == true)
+            {
+                Console.WriteLine($"Hello {name}. Nice to meet you. I see you have a dog named {dogName}. Cool name bro. It's nice to see someone at the age of {age} that has a dog.");
+            }
+            else 
+            {
+                Console.WriteLine($"What {name}!!! You don't own a dog? Someone {age} years old, should really own a dog.");
+            }
+        }
+    }
+}
+
+
---
+++ Class Workshops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Class_Workshops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myHoomans();
+        }
+        static void myHoomans()
+        {
+            HoomanClass MrSekol = new HoomanClass();
+            MrSekol.age = 99; //haha old
+            MrSekol.dogName = "Mozzy";
+            MrSekol.hasDog = true;
+            MrSekol.name = "Michael";
+
+            MrSekol.Greeting();
+        }
+    }
+}

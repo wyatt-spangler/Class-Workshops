d4106add9687be2bc38ce0a53c87f880dbf4cf46
Wyatt Spangler <wyatt.spa@outlook.com>
2020-09-08 08:22:45 -0400

    test commit

---
+++ Class Workshops/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Workshops
+{
+    class Class2
+    {
+        public string CharacterName; public int CharacterLevel; public string CharacterRace; public double HitPoints; public string CharacterClass;
+        public bool inGuild;
+
+        public void CharacterSelect()
+        {
+            if (CharacterLevel >= 100)
+            {
+                Console.WriteLine($"Welcome {CharacterName}. You are mighty {CharacterClass}, of the {CharacterRace} people. You must be a hard one to defeat with {HitPoints} hit points.");
+
+            }
+            else if (CharacterLevel >= 50)
+            { 
+            
+            }
+        }
+
+
+
+
+    }
+
+}

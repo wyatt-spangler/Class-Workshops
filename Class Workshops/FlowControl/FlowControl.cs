7e867ba1d337fd317b1b01ffb7c3c80fb89e4173
Wyatt Spangler <wyatt.spa@outlook.com>
2020-09-08 11:30:12 -0400

    added a random number generator

--- Class Workshops/FlowControl/FlowControl.cs
+++ Class Workshops/FlowControl/FlowControl.cs
@@ -11,10 +11,12 @@ namespace Class_Workshops
             public const int minimumAge = 13;
             public static int playerAge;
             public static string permission;
+            public static int computerRPSRandomNum;
 
 
             public static void FCIfElse()
             {
+           FCSwitchRoll();
             Console.WriteLine("Welcome to Rock Paper Scissors, what is your age?");
             playerAge = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Wow, You are {playerAge} years old!");
@@ -58,6 +60,35 @@ namespace Class_Workshops
                 Console.WriteLine("I did not understand that answer. Please Try Again!");
                 FCElseIf();
             }
+
+
+
+        }
+
+        public static void FCSwitchRoll()
+        {
+            Random computerRandomRPS = new Random();
+            computerRPSRandomNum = computerRandomRPS.Next(1, 4);
+            Console.WriteLine($"My Random Number is {computerRPSRandomNum}");
+
+            switch (computerRPSRandomNum)
+            {
+                case 4:
+                    Console.WriteLine("Oops PC rolled a 4. That wasn't suppose to happen.");
+                    FCSwitchRoll();
+                    break;
+                case 3:
+                    Console.WriteLine($"PC chose a {computerRPSRandomNum}. That is equal to Rock.");
+                    break;
+                case 2:
+                    Console.WriteLine($"PC chose a {computerRPSRandomNum}. That is equal to Paper.");
+                    break;
+                case 1:
+                    Console.WriteLine($"PC chose a {computerRPSRandomNum}. That is equal to Scissors");
+                    break;
+            }
+            Console.ReadLine();
+            FCSwitchRoll();
 
 
 
